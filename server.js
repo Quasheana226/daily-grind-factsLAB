f9d7522bf76123b29f4a7ede8ca4378dc851ba19
Firee <newowner@News-MacBook-Pro.local>
2026-05-14 07:34:21 -0400

    Set up basic Express server listening on PORT 1738

---
+++ server.js
@@ -0,0 +1,19 @@
+// DEPENDENCIES
+const express = require("express");
+
+const axios = require("axios");
+
+const app = express();
+
+require("dotenv").config(); // load .env file
+
+
+//MIDDLEWARE
+
+
+//ROUTES
+
+//PORT
+
+
+const PORT = process.env.PORT; //Grabbing port from .env
